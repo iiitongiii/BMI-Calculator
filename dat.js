546c9016d05fef00edef8fdb3c8d655ef60e4159
iiitongiii <59644176+iiitongiii@users.noreply.github.com>
2021-04-07 02:04:29 +0800

    Add files via upload

--- dat.js
+++ dat.js
@@ -2,7 +2,6 @@ const gui = new dat.GUI()
 
 
 var folder1 = gui.addFolder('Flow Field');
-gui.add(camera.position, 'y' , -5, 5).name('try');
 gui.add(controls, 'enabled').name('Orbit Control');
 
 gui.add(light.position, 'y' , -5,5).name('light.y');
